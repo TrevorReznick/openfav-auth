3fd632492c9e7f150076cbb86ad222460b7d7340
Tuo Nome <double.facessss@gmail.com>
2024-10-06 09:25:24 +0200

    OPF-10 added preline ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    './node_modules/preline/preline.js',
+  ],
+  plugins: [
+    require('preline/plugin'),  
+  ],
   theme: {
     extend: {
       colors: {
