7253fff7d656b53e95239e1218a1075512313d39
Vincent Maximo II <projects.manager@xdomain.it>
2024-10-06 18:26:30 +0200

    OPF-3 cleaned dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,8 @@
 module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    './node_modules/preline/preline.js',
   ],
-  plugins: [
-    require('preline/plugin'),  
-  ],
+  plugins: [],
   theme: {
     extend: {
       colors: {
