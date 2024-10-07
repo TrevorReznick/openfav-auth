c4da5252f99008a5b1a7083a9af3c1cf8bef78ed
Tuo Nome <double.facessss@gmail.com>
2024-10-07 16:39:26 +0200

    OPF-3 setting form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+export interface Form {
+    inputs?: Array<Input>
+    textarea?: Textarea
+    disclaimer?: Disclaimer
+    button?: string
+    description?: string
+}
