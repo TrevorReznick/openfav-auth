a58be8d85d979157b46280e11d66dfe0bebe0e41
Tuo Nome <double.facessss@gmail.com>
2024-10-12 09:07:50 +0200

    OPF-2 fixed provider redirect

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,6 @@
 import { defineMiddleware } from 'astro:middleware'
 import { supabase } from '~/providers/supabase'
-import { id, email, user_name } from '../store'
+import { id, email, user_name, isAuthenticated } from '../store'
 
 const protectedRoutes = ['/test/test-reusable']
 const redirectRoutes = ['/login', '/register']
@@ -32,10 +32,11 @@ export const onRequest = defineMiddleware(
       console.log('auth data', data)
 
       /* @@ set store @@ */
-
       const email_ = data.user?.email ?? ''
       email.set(email_)
       id.set(data.user?.id ?? '')
+      isAuthenticated.set(true)
+      console.log('authenticated : ', isAuthenticated.get())
       const [extractUsername] = email_.split('@')
       user_name.set(extractUsername ?? '')
       
